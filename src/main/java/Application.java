ceeafea406316d747de0844b2997685d99995b72
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-04-10 14:09:42 +0200

    implement Application class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,15 @@
-public class Application {
+import com.sun.net.httpserver.HttpsServer;
 
+import java.net.InetSocketAddress;
+
+public class Application {
+    public static void main(String[] args) throws Exception {
+        HttpsServer server = HttpsServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/hello", new Hello());
+        server.setExecutor(null);
+
+        server.start();
+
+
+    }
 }
