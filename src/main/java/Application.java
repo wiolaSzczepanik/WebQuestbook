de45b9251f39a8b8def0567ca5d9765b17ad1f32
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-04-10 15:04:14 +0200

    make work class Application work properly

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,14 @@
-import com.sun.net.httpserver.HttpsServer;
+import com.sun.net.httpserver.HttpServer;
 
 import java.net.InetSocketAddress;
 
 public class Application {
     public static void main(String[] args) throws Exception {
-        HttpsServer server = HttpsServer.create(new InetSocketAddress(8000), 0);
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
         server.createContext("/hello", new Hello());
+
         server.setExecutor(null);
 
         server.start();
